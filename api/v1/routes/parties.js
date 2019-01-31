c27fbea327f0eabf0d722483428b665730d5395d
Abhay <Abhay>
2019-01-31 02:52:20 +0000

    [Feature #163507005] implemented Create Party Endpoint

--- api/v1/routes/parties.js
+++ api/v1/routes/parties.js
@@ -2,10 +2,36 @@ const express = require('express');
 
 const router = express.Router();
 
+// const Product = require('../models/party');
+partieslist = [];
 
 //create party endpoint
-
-
+router.post('/', (req,res,next)=>{
+	const party = {
+		name: req.body.name,
+		hqAddress: req.body.hqAddress,
+		logoUrl: req.body.logoUrl
+	}
+	if(!req.body.name || !req.body.hqAddress || !req.body.logoUrl){
+		return res.status(400).json({
+			"status": "400",
+			"error":"Bad request, supply all fields"
+		});
+	}
+	let id = partieslist.length + 1;
+	partieslist.push([{
+		"id": id,
+		"name": req.body.name,
+		"logoUrl": req.body.logoUrl
+	}]);
+	res.status(200).json({
+		status: '200',
+		data: [{
+			"id": id,
+			"name": req.body.name
+		}]
+	});
+});
 
 
 
